76d7320639ec5501e5ff79437e892514a5c9b71b
Albert Sheu <albert.sheu@gmail.com>
2024-03-20 00:17:56 -0400

    Add PWA features maybe

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+
+import "./index.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
-import "./index.css";
+import App from "./App.tsx";
 
 library.add(far, fas);
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js").then(
+    function (registration) {
+      // Registration was successful
+      console.log("ServiceWorker registration successful with scope: ", registration.scope);
+    },
+    function (err) {
+      // registration failed :(
+      console.log("ServiceWorker registration failed: ", err);
+    }
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
